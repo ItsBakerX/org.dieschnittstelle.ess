d00b9c7ca19d179196035bd75393b32fdbbd7f77
ItsBakerX <s92253@bht-berlin.de>
2026-04-21 13:02:48 +0200

    BAS2, SER3

--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
@@ -3,7 +3,14 @@ package org.dieschnittstelle.ess.basics;
 
 import org.dieschnittstelle.ess.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.ess.basics.annotations.StockItemProxyImpl;
+import org.w3c.dom.NodeList;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.dieschnittstelle.ess.basics.reflection.ReflectedStockItemBuilder.getAccessorNameForField;
 import static org.dieschnittstelle.ess.utils.Utils.*;
 
 public class ShowAnnotations {
@@ -29,7 +36,10 @@ public class ShowAnnotations {
 	 * TODO BAS2
 	 */
 	private static void showAttributes(Object instance) {
-		show("class is: " + instance.getClass());
+		show("class is: " + instance.getClass().getSimpleName());
+		Class klass = instance.getClass();
+		StringBuilder sb = new StringBuilder();
+		sb.append("{").append(klass.getSimpleName()).append(" ");
 
 		try {
 
@@ -39,6 +49,20 @@ public class ShowAnnotations {
 			//  will then be built from the field names and field values.
 			//  Note that only read-access to fields via getters or direct access
 			//  is required here.
+			for (Field field : klass.getDeclaredFields()) {
+				String getterName = getAccessorNameForField("get", field.getName());
+				Method getter = klass.getMethod(getterName);
+				//invoke returns the value of a field with the getter method
+				var value = getter.invoke(instance);
+				var attr = field.getName();
+
+				sb.append(attr).append(":").append(value).append(", ");
+
+			}
+			//remove the last comma and space
+			sb.setLength(sb.length()-2);
+			sb.append("}");
+			show(sb.toString());
 
 			// TODO BAS3: if the new @DisplayAs annotation is present on a field,
 			//  the string representation will not use the field's name, but the name

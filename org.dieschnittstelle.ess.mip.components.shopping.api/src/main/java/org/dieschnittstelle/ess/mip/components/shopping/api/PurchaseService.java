5db51423fa03ade54eadb11e3fb992a5003e562a
s92253 <s92253@bht-berlin.de>
2026-07-19 16:50:52 +0200

    add annotation in PurchaseService to specify

--- org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
+++ org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
@@ -3,10 +3,13 @@ package org.dieschnittstelle.ess.mip.components.shopping.api;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+// TODO: PAT1: this is the interface to be provided as a rest service if rest service access is used
 @Path("/purchase")
 @Consumes({MediaType.APPLICATION_JSON})
+@Produces(MediaType.APPLICATION_JSON)
 public interface PurchaseService {
 	@POST
 	void purchaseCartAtTouchpointForCustomer(PurchaseDTO purchaseDTO) throws ShoppingException;
